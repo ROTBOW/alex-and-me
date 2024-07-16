13e32605ba9a0409d3fbdb69d905172679b34d7c
Josiah Leon <sjosiahleon@gmail.com>
2024-07-15 18:30:36 -0700

    project init!

---
+++ my_to_do_frontend/app/test/page.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const Test = () => {
+
+    return (
+        <div className="text-lg flex justify-center mt-4">
+            hey there soul sister!
+        </div>
+    )
+}
+
+export default Test;
